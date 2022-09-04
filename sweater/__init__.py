82e41a27c00c9f4b0d5d8ee2870dec266c105c73
YxTiBlya <karasevstepan674@gmail.com>
2022-09-04 17:23:04 +0400

    add env variables

--- sweater/__init__.py
+++ sweater/__init__.py
@@ -8,13 +8,14 @@ BASE_DIR = os.path.abspath(os.curdir)
 UPLOAD_FOLDER =  BASE_DIR + '\sweater\static\img'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
 
-print('BASE-DIR: ' + BASE_DIR)
-print('UPLOAD_FOLDER-DIR: ' + UPLOAD_FOLDER)
-
 app = Flask(__name__)
 app.secret_key = 'SUPER PUPER SECRET SALT'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Lalka12322@localhost/flask-webshop'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@db/{os.environ.get('POSTGRES_DB')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 manager = LoginManager(app)
+
+print('BASE-DIR: ' + BASE_DIR)
+print('UPLOAD_FOLDER-DIR: ' + UPLOAD_FOLDER)
+print('DATABASE_URL: ' + app.config['SQLALCHEMY_DATABASE_URI'])
